8e189bdaeafc224e5a671b84ab01b7bc01fdaa67
riyalohia <riyalh1997@gmail.com>
2023-07-22 19:46:55 +0530

    Add links to left sidebar

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,9 +1,15 @@
+import { sidebarLinks } from '@/constants'
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const LeftSidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
     <section className={cn("left_sidebar h-[calc(100vh-5px)]")}>
       <nav className="flex flex-col gap-6">
@@ -11,6 +17,16 @@ const LeftSidebar = () => {
           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
           <h1 className="text-24 font-extrabold text-white max-lg:hidden">Podcastr</h1>
         </Link>
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive = pathname === route || pathname.startsWith(`${route}/`);
+
+          return <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
+            'bg-nav-focus border-r-4 border-orange-1': isActive
+          })}>
+            <Image src={imgURL} alt={label} width={24} height={24} />
+            <p>{label}</p>
+          </Link>
+        })}
       </nav>  
     </section>
   )

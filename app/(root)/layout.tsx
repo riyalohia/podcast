602e8f07042bcffece09c7f3c2c57429d41a513f
riyalohia <riyalh1997@gmail.com>
2023-07-27 19:54:54 +0530

    Fixes RIght Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
 
 export default function RootLayout({
@@ -23,6 +24,8 @@ export default function RootLayout({
 						</div>
 					</div>
 				</section>
+
+				<RightSidebar />
 			</main>
 		</div>
 	);

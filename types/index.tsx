5c29f00f409c7d0283cf13e5c29a5c86411bfc44
riyalohia <riyalh1997@gmail.com>
2023-08-07 20:08:09 +0530

    Adds podcastCard type

---
+++ types/index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+
+export interface LatestPodcastCardProps {
+  imgUrl: string;
+  title: string;
+  duration: string;
+  index: number;
+  audioUrl: string;
+  author: string;
+  views: number;
+}

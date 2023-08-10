a0ff560c18096409824b03293b193c9cfe8ba276
riyalohia <riyalh1997@gmail.com>
2023-08-10 20:12:36 +0530

    Adds podcast list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,24 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+"use client";
+import PodcastCard from '@/components/PodcastCard'
+import { podcastData } from '@/constants';
 
 const Home = () => {
   return (
-    <div className="mt-9 flex flex-col gap-9">
-      <section className="flex flex-col gap-5">
+    <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
+      <section className='flex flex-col gap-5'>
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-        <Button className="text-white-1">Test Me</Button>
+
+        <div className="podcast_grid">
+          {podcastData?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+            <PodcastCard 
+              key={_id}
+              imgUrl={imageUrl as string}
+              title={podcastTitle}
+              description={podcastDescription}
+              podcastId={_id}
+            />
+          ))}
+        </div>
       </section>
     </div>
   )

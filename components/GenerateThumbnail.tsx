455bb6f5bdeb62fde2585753aabbe823a345ce5c
riyalohia <riyalh1997@gmail.com>
2023-09-08 22:24:28 +0530

    Adds generate thumbnail comp

---
+++ components/GenerateThumbnail.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GenerateThumbnail = () => {
+	return (
+		<div>
+			<h1 className="text-20 font-bold text-white-1">GenerateThumbnail</h1>
+		</div>
+	)
+}
+
+export default GenerateThumbnail

b824051d33cda612340c47581da31261f140beb4
riyalohia <riyalh1997@gmail.com>
2023-09-08 22:23:45 +0530

    Adds generate podcast comp

---
+++ components/GeneratePodcast.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GeneratePodcast = () => {
+	return (
+		<div>
+			<h1 className="text-20 font-bold text-white-1">GeneratePodcast</h1>
+		</div>
+	)
+}
+
+export default GeneratePodcast

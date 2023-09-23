df32b3169b48cc5743d903956096c64aeca9e30e
riyalohia <riyalh1997@gmail.com>
2023-09-23 22:33:13 +0530

    Updates types

--- types/index.tsx
+++ types/index.tsx
@@ -1,5 +1,22 @@
+/* eslint-disable no-unused-vars */
+
+import { Dispatch, SetStateAction } from "react";
+
+import { Id } from "@/convex/_generated/dataModel";
+
 export interface PodcastCardProps {
 	imgUrl: string;
 	title: string;
 	description: string;
+	podcastId: Id<"podcasts">;
 }
+
+export interface GeneratePodcastProps {
+	voiceType: string;
+	setAudio: Dispatch<SetStateAction<string>>;
+	audio: string;
+	setAudioStorageId: Dispatch<SetStateAction<Id<"_storage"> | null>>;
+	voicePrompt: string;
+	setVoicePrompt: Dispatch<SetStateAction<string>>;
+	setAudioDuration: Dispatch<SetStateAction<number>>;
+  }

2a90c40bdf4fbe81edc977e01f57a2508e663446
riyalohia <riyalh1997@gmail.com>
2023-10-05 22:48:37 +0530

    Adds generate podcast component

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -1,11 +1,46 @@
-import React from "react";
+import { GeneratePodcastProps } from '@/types'
+import React, { useState } from 'react'
+import { Label } from './ui/label'
+import { Textarea } from './ui/textarea'
+import { Button } from './ui/button'
+import { Loader } from 'lucide-react'
+import { useAction, useMutation } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { v4 as uuidv4 } from 'uuid';
+import { useToast } from "@/components/ui/use-toast"
 
-const GeneratePodcast = () => {
-	return (
-		<div>
-			<h1 className="text-20 font-bold text-white-1">GeneratePodcast</h1>
-		</div>
-	)
+import { useUploadFiles } from '@xixixao/uploadstuff/react';
+
+
+const GeneratePodcast = (props: GeneratePodcastProps) => {
+
+  return (
+    <div>
+      <div className="flex flex-col gap-2.5">
+        <Label className="text-16 font-bold text-white-1">
+          AI Prompt to generate Podcast
+        </Label>
+        <Textarea 
+          className="input-class font-light focus-visible:ring-offset-orange-1"
+          placeholder='Provide text to generate audio'
+          rows={5}
+          value={props.voicePrompt}
+          onChange={(e) => props.setVoicePrompt(e.target.value)}
+        />
+      </div>
+      <div className="mt-5 w-full max-w-[200px]">
+      </div>
+      {props.audio && (
+        <audio 
+          controls
+          src={props.audio}
+          autoPlay
+          className="mt-5"
+          onLoadedMetadata={(e) => props.setAudioDuration(e.currentTarget.duration)}
+        />
+      )}
+    </div>
+  )
 }
 
 export default GeneratePodcast

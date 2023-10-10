6fea4b170480ed2ff54548828da3092cc22b4d6d
riyalohia <riyalh1997@gmail.com>
2023-10-10 22:52:23 +0530

    Adds generate podcast hook

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -11,8 +11,67 @@ import { useToast } from "@/components/ui/use-toast"
 
 import { useUploadFiles } from '@xixixao/uploadstuff/react';
 
+const useGeneratePodcast = ({
+  setAudio, voiceType, voicePrompt, setAudioStorageId
+}: GeneratePodcastProps) => {
+  const [isGenerating, setIsGenerating] = useState(false);
+  const { toast } = useToast()
+
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+  const { startUpload } = useUploadFiles(generateUploadUrl)
+
+  const getPodcastAudio = useAction(api.openai.generateAudioAction)
+
+  const getAudioUrl = useMutation(api.podcasts.getUrl);
+
+  const generatePodcast = async () => {
+    setIsGenerating(true);
+    setAudio('');
+
+    if(!voicePrompt) {
+      toast({
+        title: "Please provide a voiceType to generate a podcast",
+      })
+      return setIsGenerating(false);
+    }
+
+    try {
+      const response = await getPodcastAudio({
+        voice: voiceType,
+        input: voicePrompt
+      })
+
+      const blob = new Blob([response], { type: 'audio/mpeg' });
+      const fileName = `podcast-${uuidv4()}.mp3`;
+      const file = new File([blob], fileName, { type: 'audio/mpeg' });
+
+      const uploaded = await startUpload([file]);
+      const storageId = (uploaded[0].response as any).storageId;
+
+      setAudioStorageId(storageId);
+
+      const audioUrl = await getAudioUrl({ storageId });
+      setAudio(audioUrl!);
+      setIsGenerating(false);
+      toast({
+        title: "Podcast generated successfully",
+      })
+    } catch (error) {
+      console.log('Error generating podcast', error)
+      toast({
+        title: "Error creating a podcast",
+        variant: 'destructive',
+      })
+      setIsGenerating(false);
+    }
+    
+  }
+
+  return { isGenerating, generatePodcast }
+}
 
 const GeneratePodcast = (props: GeneratePodcastProps) => {
+  const { isGenerating, generatePodcast } = useGeneratePodcast(props);
 
   return (
     <div>
@@ -29,6 +88,16 @@ const GeneratePodcast = (props: GeneratePodcastProps) => {
         />
       </div>
       <div className="mt-5 w-full max-w-[200px]">
+      <Button type="submit" className="text-16 bg-orange-1 py-4 font-bold text-white-1" onClick={generatePodcast}>
+        {isGenerating ? (
+          <>
+            Generating
+            <Loader size={20} className="animate-spin ml-2" />
+          </>
+        ) : (
+          'Generate'
+        )}
+      </Button>
       </div>
       {props.audio && (
         <audio 

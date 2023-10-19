5536848071b3220259501404af20a7db444a421f
riyalohia <riyalh1997@gmail.com>
2023-10-19 23:06:00 +0530

    Adds empty state types

--- types/index.tsx
+++ types/index.tsx
@@ -4,6 +4,13 @@ import { Dispatch, SetStateAction } from "react";
 
 import { Id } from "@/convex/_generated/dataModel";
 
+export interface EmptyStateProps {
+  title: string;
+  search?: boolean;
+  buttonText?: string;
+  buttonLink?: string;
+}
+
 export interface PodcastCardProps {
 	imgUrl: string;
 	title: string;

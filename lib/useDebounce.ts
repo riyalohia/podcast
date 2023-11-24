757a8772d1f6eadb60b43098309ec26f1b9f4972
riyalohia <riyalh1997@gmail.com>
2023-11-24 23:38:10 +0530

    Fix

--- lib/useDebounce.ts
+++ lib/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const useDebounce = <T>(value: T, delay) => {
+export const useDebounce = <T>(value: T, delay = 300) => {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {

0afa96f1210a993b41f0aa2cb9e0bc9dbb859fe1
riyalohia <riyalh1997@gmail.com>
2023-12-11 23:54:44 +0530

    Adds carousel

---
+++ components/Carousel.tsx
@@ -0,0 +1,44 @@
+import React, { useCallback } from 'react'
+import { EmblaOptionsType, EmblaCarouselType } from 'embla-carousel'
+import { DotButton, useDotButton } from './EmblaCarouselDotButton'
+import Autoplay from 'embla-carousel-autoplay'
+import useEmblaCarousel from 'embla-carousel-react'
+import { CarouselProps } from '@/types'
+import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+import LoaderSpinner from './LoaderSpinner'
+
+const EmblaCarousel = ({ fansLikeDetail }: CarouselProps) => {
+  const router = useRouter();
+
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()])
+
+  const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
+    const autoplay = emblaApi?.plugins()?.autoplay
+    if (!autoplay || !("stopOnInteraction" in autoplay.options)) return
+
+    const resetOrStop =
+      autoplay.options.stopOnInteraction === false
+        ? (autoplay.reset as () => void)
+        : (autoplay.stop as () => void)
+
+    resetOrStop()
+  }, [])
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(
+    emblaApi,
+    onNavButtonClick
+  )
+
+  const slides = fansLikeDetail && fansLikeDetail?.filter((item: any) => item.totalPodcasts > 0)
+
+  if(!slides) return <LoaderSpinner />
+
+  return (
+    <section className="flex w-full flex-col gap-4 overflow-hidden" ref={emblaRef}>
+      
+    </section>
+  )
+}
+
+export default EmblaCarousel

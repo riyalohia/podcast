1decc5a916647f324810e684defd622316ab184e
riyalohia <riyalh1997@gmail.com>
2023-12-15 23:56:38 +0530

    Adds sidebar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+'use client';
+
+import { SignedIn, UserButton, useUser } from '@clerk/nextjs'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+import Header from './Header';
+import Carousel from './Carousel';
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { useRouter } from 'next/navigation';
+import LoaderSpinner from './LoaderSpinner';
+import { useAudio } from '@/providers/AudioProvider';
+import { cn } from '@/lib/utils';
 
 const RightSidebar = () => {
-	return (
-		<div>Right Sidebar</div>
-	)
+  const { user } = useUser();
+  const topPodcasters = useQuery(api.users.getTopUserByPodcastCount);
+  const router = useRouter();
+
+  const { audio } = useAudio();
+
+  return (
+    <section className={cn('right_sidebar h-[calc(100vh-5px)]', {
+      'h-[calc(100vh-140px)]': audio?.audioUrl
+    })}>
+      <section>
+        <Header headerTitle="Fans Like You" />
+        <Carousel fansLikeDetail={topPodcasters!}/>
+      </section>
+      <section className="flex flex-col gap-8 pt-12">
+        <Header headerTitle="Top Podcastrs" />
+        <div className="flex flex-col gap-6">
+        </div>
+      </section>
+    </section>
+  )
 }
 
 export default RightSidebar

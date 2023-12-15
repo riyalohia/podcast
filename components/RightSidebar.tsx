6f3b14729775b307ae40b37fb2171940b914704e
riyalohia <riyalh1997@gmail.com>
2023-12-15 23:57:32 +0530

    Modifies right side bar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -24,6 +24,20 @@ const RightSidebar = () => {
     <section className={cn('right_sidebar h-[calc(100vh-5px)]', {
       'h-[calc(100vh-140px)]': audio?.audioUrl
     })}>
+      <SignedIn>
+        <Link href={`/profile/${user?.id}`} className="flex gap-3 pb-12">
+          <UserButton />
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-16 truncate font-semibold text-white-1">{user?.firstName} {user?.lastName}</h1>
+            <Image 
+              src="/icons/right-arrow.svg"
+              alt="arrow"
+              width={24}
+              height={24}
+            />
+          </div>
+        </Link>
+      </SignedIn>
       <section>
         <Header headerTitle="Fans Like You" />
         <Carousel fansLikeDetail={topPodcasters!}/>

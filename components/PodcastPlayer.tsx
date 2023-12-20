1b3d1de687fcc6e905b64bda3dc85ec182871030
riyalohia <riyalh1997@gmail.com>
2023-12-21 00:03:46 +0530

    Adds podcast player comp

---
+++ components/PodcastPlayer.tsx
@@ -0,0 +1,113 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+
+import { formatTime } from "@/lib/formatTime";
+import { cn } from "@/lib/utils";
+import { useAudio } from "@/providers/AudioProvider";
+
+import { Progress } from "./ui/progress";
+
+const PodcastPlayer = () => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [isMuted, setIsMuted] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const { audio } = useAudio();
+
+  const togglePlayPause = () => {
+    if (audioRef.current?.paused) {
+      audioRef.current?.play();
+      setIsPlaying(true);
+    } else {
+      audioRef.current?.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  const toggleMute = () => {
+    if (audioRef.current) {
+      audioRef.current.muted = !isMuted;
+      setIsMuted((prev) => !prev);
+    }
+  };
+
+  const forward = () => {
+    if (
+      audioRef.current &&
+      audioRef.current.currentTime &&
+      audioRef.current.duration &&
+      audioRef.current.currentTime + 5 < audioRef.current.duration
+    ) {
+      audioRef.current.currentTime += 5;
+    }
+  };
+
+  const rewind = () => {
+    if (audioRef.current && audioRef.current.currentTime - 5 > 0) {
+      audioRef.current.currentTime -= 5;
+    } else if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+    }
+  };
+
+  useEffect(() => {
+    const updateCurrentTime = () => {
+      if (audioRef.current) {
+        setCurrentTime(audioRef.current.currentTime);
+      }
+    };
+
+    const audioElement = audioRef.current;
+    if (audioElement) {
+      audioElement.addEventListener("timeupdate", updateCurrentTime);
+
+      return () => {
+        audioElement.removeEventListener("timeupdate", updateCurrentTime);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    const audioElement = audioRef.current;
+    if (audio?.audioUrl) {
+      if (audioElement) {
+        audioElement.play().then(() => {
+          setIsPlaying(true);
+        });
+      }
+    } else {
+      audioElement?.pause();
+      setIsPlaying(true);
+    }
+  }, [audio]);
+  const handleLoadedMetadata = () => {
+    if (audioRef.current) {
+      setDuration(audioRef.current.duration);
+    }
+  };
+
+  const handleAudioEnded = () => {
+    setIsPlaying(false);
+  };
+
+  return (
+    <div
+      className={cn("sticky bottom-0 left-0 flex size-full flex-col", {
+        hidden: !audio?.audioUrl || audio?.audioUrl === "",
+      })}
+    >
+      {/* change the color for indicator inside the Progress component in ui folder */}
+      <Progress
+        value={(currentTime / duration) * 100}
+        className="w-full"
+        max={duration}
+      />
+
+    </div>
+  );
+};
+
+export default PodcastPlayer;

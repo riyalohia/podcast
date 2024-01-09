c3abe8139566ba5b7d956823cb34d6cbe167e42b
riyalohia <riyalh1997@gmail.com>
2024-01-09 16:52:49 +0530

    Displays podcast list

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -19,6 +19,23 @@ const Discover = ({ searchParams: { search} }: { searchParams : { search: string
           {!search ? 'Discover Trending Podcasts' : 'Search results for '}
           {search && <span className="text-white-2">{search}</span>}
         </h1>
+        {podcastsData ? (
+          <>
+            {podcastsData.length > 0 ? (
+              <div className="podcast_grid">
+              {podcastsData?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+                <PodcastCard 
+                  key={_id}
+                  imgUrl={imageUrl!}
+                  title={podcastTitle}
+                  description={podcastDescription}
+                  podcastId={_id}
+                />
+              ))}
+            </div>
+            ) : <EmptyState title="No results found" />}
+          </>
+        ) : <LoaderSpinner />}
       </div>
     </div>
   )

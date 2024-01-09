6a752996303743e5cd0df6de4188cbcc8fab655b
riyalohia <riyalh1997@gmail.com>
2024-01-09 16:52:21 +0530

    Adds search query

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -15,7 +15,10 @@ const Discover = ({ searchParams: { search} }: { searchParams : { search: string
     <div className="flex flex-col gap-9">
       <Searchbar />
       <div className="flex flex-col gap-9">
-        
+        <h1 className="text-20 font-bold text-white-1">
+          {!search ? 'Discover Trending Podcasts' : 'Search results for '}
+          {search && <span className="text-white-2">{search}</span>}
+        </h1>
       </div>
     </div>
   )

f89bbe5a43e43e6d650311967d49efbe7c05b03e
riyalohia <riyalh1997@gmail.com>
2024-01-16 16:55:30 +0530

    Adds mobile nav layout

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+"use client"
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { sidebarLinks } from "@/constants"
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
 
 const MobileNav = () => {
-	return (
-		<div>Mobile Nav</div>
-	)
+  const pathname = usePathname();
+
+  return (
+    <section>
+     
+    </section>
+  )
 }
 
 export default MobileNav

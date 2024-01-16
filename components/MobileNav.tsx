4b731275c21bc934b10c20b319c03fd44dbcd586
riyalohia <riyalh1997@gmail.com>
2024-01-16 16:55:56 +0530

    Adds mobile nav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -18,7 +18,17 @@ const MobileNav = () => {
 
   return (
     <section>
-     
+      <Sheet>
+        <SheetTrigger>
+          <Image src="/icons/hamburger.svg" width={30} height={30} alt="menu" className="cursor-pointer" />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-black-1">
+          <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10 pl-4">
+            <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+            <h1 className="text-24 font-extrabold  text-white-1 ml-2">Podcastr</h1>
+          </Link>
+        </SheetContent>
+      </Sheet>
     </section>
   )
 }

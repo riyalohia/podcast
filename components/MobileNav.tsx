ab9938ac63ded4aa78b0ee5c0fdece8d3d27687d
riyalohia <riyalh1997@gmail.com>
2024-01-21 16:56:21 +0530

    Adds mobile nav comp

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -27,6 +27,22 @@ const MobileNav = () => {
             <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
             <h1 className="text-24 font-extrabold  text-white-1 ml-2">Podcastr</h1>
           </Link>
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 text-white-1">
+              {sidebarLinks.map(({ route, label, imgURL }) => {
+                const isActive = pathname === route || pathname.startsWith(`${route}/`);
+
+                return <SheetClose asChild key={route}><Link href={route} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-start", {
+                  'bg-nav-focus border-r-4 border-orange-1': isActive
+                })}>
+                  <Image src={imgURL} alt={label} width={24} height={24} />
+                  <p>{label}</p>
+                </Link></SheetClose>
+              })}
+              </nav>
+            </SheetClose>
+          </div>
         </SheetContent>
       </Sheet>
     </section>

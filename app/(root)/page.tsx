d87e93df05d35a2256798a6716fc0e7dd09bb517
riyalohia <riyalh1997@gmail.com>
2024-01-21 16:59:03 +0530

    Fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,17 +2,20 @@
 import PodcastCard from '@/components/PodcastCard'
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
-import { podcastData } from '@/constants';
+import LoaderSpinner from '@/components/LoaderSpinner';
 
 const Home = () => {
+  const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
 
+  if(!trendingPodcasts) return <LoaderSpinner />
+  
   return (
     <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
       <section className='flex flex-col gap-5'>
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
 
         <div className="podcast_grid">
-          {podcastData?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+          {trendingPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
             <PodcastCard 
               key={_id}
               imgUrl={imageUrl as string}
